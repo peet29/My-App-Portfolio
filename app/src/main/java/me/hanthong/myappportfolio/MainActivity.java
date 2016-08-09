3b3e4b62a910efd045e9f58ef4b0379fb1450a6f
Thanawat Hanthong <peet6015@gmail.com>
2016-08-09 13:25:45 +0700

    make button open app

--- app/src/main/java/me/hanthong/myappportfolio/MainActivity.java
+++ app/src/main/java/me/hanthong/myappportfolio/MainActivity.java
@@ -1,6 +1,8 @@
 package me.hanthong.myappportfolio;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -38,29 +40,61 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void showToast(View view) {
+    public void StartNewApp(View view) {
         Context context = getApplicationContext();
         String appName = "";
 
         switch (view.getId()) {
             case R.id.button_spotify:
-                appName = "Spotify Streamer";
+                appName = "me.hanthong.android.popularmovies";
                 break;
             case R.id.button_super:
-                appName = "Super Duo";
+                appName = "com.sam_chordas.android.stockhawk";
                 break;
             case R.id.button_buildit:
-                appName = "Build It Bigger";
+                appName = "com.udacity.gradle.builditbigger";
                 break;
             case R.id.button_xyz:
-                appName = "XYZ Reader";
+                appName = "com.example.xyzreader";
                 break;
             case R.id.button_capstone:
-                appName = "Capstone";
+                appName = "me.hanthong.capstone";
                 break;
         }
+
+        if(!openApp(this,appName)) {
+            showToast(appName);
+        }
+    }
+
+    /**
+     * Open another app.
+     *
+     * @param context     current Context, like Activity, App, or Service
+     * @param packageName the full package name of the app to open
+     * @return true if likely successful, false if unsuccessful
+     */
+    public static boolean openApp(Context context, String packageName) {
+        PackageManager manager = context.getPackageManager();
+        try {
+            Intent i = manager.getLaunchIntentForPackage(packageName);
+            if (i == null) {
+                throw new PackageManager.NameNotFoundException();
+            }
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+            context.startActivity(i);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    private void showToast(String appName)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = appName+" Not Found!";
         int duration = Toast.LENGTH_SHORT;
-        CharSequence text = "This button will launch my " + appName + " app!";
+
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
     }
